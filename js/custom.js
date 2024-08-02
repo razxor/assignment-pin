328730bf3900ae442bcd04481e5747f505ff3677
Raz Ahamed <raz.abcoder@gmail.com>
2024-08-02 23:12:21 +0600

    finishing done

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Generate PIN
+    var pin = 0;
     const button = document.querySelector('.js-generate-key');
+    const alert_box = document.querySelector('.alert');
     button.addEventListener('click', function () {        
         const input = document.querySelector('.js-pin-output');
         const secretKey = Math.floor(100000 + Math.random() * 900000); // Generates a 4-digit random number
-        input.value = secretKey;       
+        input.value = secretKey;   
+        pin = secretKey;    
     });
 
     // Keypad manage
@@ -15,14 +18,34 @@ document.addEventListener('DOMContentLoaded', function () {
         key.addEventListener('click', function () {
             const keyValue = this.textContent;
             if (keyValue === 'x') {
-                pinInput.value = pinInput.value.slice(0, -1); // Remove the last character
+                pinInput.value = pinInput.value.slice(0, -1); // Remove the last digit
             } else if (keyValue === 'Clear') {
                 pinInput.value = ''; 
-            } else if (keyValue === 'Submit') {               
-                alert('PIN Submitted: ' + pinInput.value);
+                alert_box.style.display = 'none';
+            } else if (keyValue === 'Submit') {      
+                if (pinInput.value.length ==6) {
+                    //alert('PIN Submitted: ' + pinInput.value);
+                    if(pinInput.value==pin){   
+                        alert_box.style.display = 'block';                     
+                        alert_box.classList.remove("error");
+                        alert_box.classList.add("success");
+                        alert_box.innerHTML = '<i class="fas fa-check-circle"></i> Success! The Secret Key you have entered matched.';
+                    } else{
+                        alert_box.style.display = 'block';   
+                        alert_box.classList.remove("success");
+                        alert_box.classList.add("error");
+                        alert_box.innerHTML = '<i class="far fa-times-circle"></i> Alert! The Secret Key you have entered does not matched.';
+                    }
+                } else {
+                    alert_box.style.display = 'block';   
+                    alert_box.classList.remove("success");
+                    alert_box.classList.add("error");
+                    alert_box.innerHTML = '<i class="far fa-times-circle"></i> PIN Should be 6 digit';
+                }                        
+
             } else {
                 if (pinInput.value.length < 6) {
-                    pinInput.value += keyValue; 
+                    pinInput.value += keyValue;                     
                 }
             }
         });
